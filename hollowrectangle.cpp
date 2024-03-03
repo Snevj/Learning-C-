9ef3f0835527ad8599f2bbc66e85310b86d0fad3
Snevj <krishanagopal236606@gmail.com>
2024-03-04 04:28:39 +0530

    Solving the rectangle problem

---
+++ hollowrectangle.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row, col;
+    cin>>row>>col>>endl;
+    cout<<"Input the number of rows and columns you want to add"<<endl
+}
--- rectangle.cpp
+++ rectangle.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     int row, col;
+    cout<<"input row and column number respectively"<<endl;
     cin>>row>>col;
     for (int i = 1; i <= row; i++){
         for (int j = 1; j <= col ; j++)
