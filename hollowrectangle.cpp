3e4ed166de135587da0ae273c81092b5a451a07c
Snevj <krishanagopal236606@gmail.com>
2024-03-04 04:48:41 +0530

    making the pyramid

--- hollowrectangle.cpp
+++ hollowrectangle.cpp
@@ -6,7 +6,7 @@ int main(){
     cout<<"Input the number of rows and columns you want to add"<<endl;
     cin>>row>>col;
     for (int i=1;i<=row;i++){
-        for(int j=1;j<=1;j++){
+        for(int j=1;j<=i;j++){
             if(i==1||i==row||j==1||j==col){
                 cout<<"*";
             }
