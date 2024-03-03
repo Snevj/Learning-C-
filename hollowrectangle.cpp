cc5ad78b03ecd592696f4404ff42488f96851030
Snevj <krishanagopal236606@gmail.com>
2024-03-04 04:38:53 +0530

    solving the hollow rectangle problem

--- hollowrectangle.cpp
+++ hollowrectangle.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main(){
     int row, col;
-    cin>>row>>col>>endl;
-    cout<<"Input the number of rows and columns you want to add"<<endl
+    cout<<"Input the number of rows and columns you want to add"<<endl;
+    cin>>row>>col;
+    for (int i=1;i<=row;i++){
+        for(int j=1;j<=1;j++){
+            if(i==1||i==row||j==1||j==col){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }            
+        }
+        cout<<endl;
+    }
+    return 0;
+
 }
